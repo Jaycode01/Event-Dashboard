70abb74fe4947795deadfd28cb0bfc8080ae6961
Jaycode01 <josephlamidi01@gmail.com>
2025-10-01 23:56:18 +0100

    newest uncompleted update on beacon

---
+++ data/events.js
@@ -0,0 +1,72 @@
+export const Events = {
+  "category-01": {
+    "category-name": "Entertainment & Arts",
+    events: [
+      "Concerts & Music",
+      "Theatre & Performing Arts",
+      "Comedy Shows",
+      "Film & Movies",
+      "Festivals",
+      "Nightlife & Parties",
+    ],
+  },
+  "category-02": {
+    "category-name": "Learning & Professional",
+    events: [
+      "Conferences & Summits",
+      "Workshops & Classes",
+      "Seminars & Webinars",
+      "Networking Events",
+      "Trade Shows & Expos",
+    ],
+  },
+  "category-03": {
+    "category-name": "Sports & Fitness",
+    events: [
+      "Sports Events",
+      "Marathons & Races",
+      "Fitness & Wellness",
+      "Outdoor & Adventure",
+    ],
+  },
+  "category-04": {
+    "category-name": "Food & Drink",
+    events: [
+      "Food Festivals",
+      "Wine Tastings",
+      "Restaurant Events",
+      "Cooking Classes",
+    ],
+  },
+  "category-05": {
+    "category-name": "Community & Culture",
+    events: [
+      "Community Events",
+      "Charity & Fundraisers",
+      "Religious & Spiritual",
+      "Cultural Celebrations",
+    ],
+  },
+  "category-06": {
+    "category-name": "Business & Tech",
+    events: [
+      "Startup Events",
+      "Tech Meetups",
+      "Product Launches",
+      "Business Conferences",
+    ],
+  },
+  "category-07": {
+    "category-name": "Family & Education",
+    events: ["Kids & Family", "Educational Events", "Science & Tech"],
+  },
+  "category-08": {
+    "category-name": "Others",
+    events: [
+      "Virtual Events",
+      "Social Gathering",
+      "Gaming & Hobbies",
+      "Fashion & Beauty",
+    ],
+  },
+};
---
+++ script.js
@@ -0,0 +1,11 @@
+import { Events } from "./data/events.js";
+
+const discoverEvents = document.querySelector(".events");
+discoverEvents.innerHTML = `
+
+`
+
+const getEvents = () => {
+  let events;
+  events = Events.  
+}
