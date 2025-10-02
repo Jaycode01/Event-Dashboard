355ad355367ea7721f4132351cc58205e79462c8
Jaycode01 <josephlamidi01@gmail.com>
2025-10-02 02:19:14 +0100

    code + design the discover dropdown for the discover link in the navbar

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -6,25 +6,94 @@ import arrow_right from "../../assets/arrow-right.svg";
 import "./Header.css";
 
 function Header() {
+  const eventsData = [
+    {
+      category: "Entertainment & Art",
+      events: [
+        "Concerts & Music",
+        "Theatre & Performing Arts",
+        "Comedy Shows",
+        "Film & Movies",
+        "Festivals",
+        "Nightlife & Parties",
+      ],
+    },
+    {
+      category: "Tech & Business",
+      events: [
+        "Hackatons",
+        "Startup Events",
+        "Tech Meetups",
+        "Product Launches",
+        "Business Conferences",
+      ],
+    },
+    {
+      category: "Learning & Professional",
+      events: [
+        "Conferences & Summits",
+        "Workshops & classNamees",
+        "Seminars & Webinars",
+        "Networking Events",
+        "Trade Shows & Expos",
+      ],
+    },
+    {
+      category: "Community & Culture",
+      events: [
+        "Community Events",
+        "Charity & Fundraisers",
+        "Religious & Spiritual",
+        "Cultural Celebrations",
+      ],
+    },
+    {
+      category: "Sports & Fitness",
+      events: [
+        "Sports Events",
+        "Marathons & Races",
+        "Fitness & Wellness",
+        "Outdoor & Adventure",
+      ],
+    },
+    {
+      category: "Others",
+      events: [
+        "Virtual Events",
+        "Social Gathering",
+        "Gaming & Hobbies",
+        "Fashion & Beauty",
+      ],
+    },
+  ];
+
   return (
     <header>
-      <section class="left-side-header">
-        <div class="logo">
+      <section className="left-side-header">
+        <div className="logo">
           <img src={beacon} alt="beacon logo" />
         </div>
         <nav>
-          <ul class="nav-links">
-            <li class="discover">
+          <ul className="nav-links">
+            <li className="discover">
               <a href="#">
                 Discover <img src={downward_caret} alt="caret down" />
               </a>
-              <div class="discover-dropdown">
-                <ul class="select-title">
-                  <li>
-                    <a href="#">Category</a>
-                  </li>
-                </ul>
-                <ul class="events"></ul>
+              <div className="discover-dropdown">
+                {eventsData.map((eventByCategory, index) => (
+                  <div key={index} className="select-event">
+                    <h4 className="event_category">
+                      {eventByCategory.category}
+                    </h4>
+                    <ul className="events">
+                      {eventByCategory.events.map((event, index) => (
+                        <li key={index}>
+                          <a href="#">{event}</a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
               </div>
             </li>
             <li>
@@ -36,16 +105,16 @@ function Header() {
           </ul>
         </nav>
       </section>
-      <section class="right-side-header">
-        <div class="search-bar">
+      <section className="right-side-header">
+        <div className="search-bar">
           <input type="search" placeholder="Discover your next experience..." />
           <button type="button">
             <img src={search} alt="search" />
           </button>
         </div>
-        <div class="cta">
-          <button class="login">Login</button>
-          <button class="signup">
+        <div className="cta">
+          <button className="login">Login</button>
+          <button className="signup">
             Try for Free <img src={arrow_right} alt="arrow right" />
           </button>
         </div>
